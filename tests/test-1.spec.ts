1e7ad150ea25eb9fa13a1f35e3875457f63098af
Gustavo <gustavobrunoronahuz@gmail.com>
2023-06-21 19:36:56 -0300

    Correção da sequência de passos

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -6,7 +6,6 @@ test('test', async ({ page }) => {
   await page.locator('input[name="name"]').fill('ihc');
   await page.locator('input[name="password"]').click();
   await page.locator('input[name="password"]').fill('ihc');
-  await page.goto('https://inf15979.disciplinas.rlaiola.inf.ufes.br/boca/index.php');
   await page.getByRole('button', { name: 'Login' }).click();
 
   await expect(page).toHaveTitle(/Team/);
